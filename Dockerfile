07c0755759ca6488d2568ab6aad1612c5b197607
Maxonex <oluwamomis@gmail.com>
2025-01-17 01:03:33 +0100

    port adde

--- Dockerfile
+++ Dockerfile
@@ -25,4 +25,5 @@ RUN pip install --upgrade pip && pip install -r requirements.txt
 EXPOSE 8000
 
 # Command to run the app
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["sh", "-c", "python manage.py runserver 0.0.0.0:$PORT"]
+
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
-# SECURITY WARNING: don't run with debug turned on in production!
+
 if ENVIRONMENT=='development':
     DEBUG = True
 else:
