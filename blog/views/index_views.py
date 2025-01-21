6b2f02964030b66f524d3f64175de1d4913031a9
Maxonex <oluwamomis@gmail.com>
2025-01-21 13:14:09 +0100

    restructure and static

--- blog/views/index_views.py
+++ blog/views/index_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.utils import timezone
 from random import choice, sample
 from datetime import timedelta, datetime
@@ -6,82 +6,62 @@ from itertools import chain
 
 from advert.models import AdBanner
 from shop.models import Product
-from blog.models import BlogPost, Category, Trend, Video  # assuming these are your models
+from blog.models import BlogPost, Category, Trend, Video
 
 def index(request):
-    exclusive_category, global_news_category, videos_category = None, None, None
+    def get_category_posts(category_name, model=BlogPost, limit=None):
+        try:
+            category = Category.objects.get(name=category_name)
+            return model.objects.filter(category=category).order_by('-date')[:limit], category
+        except Category.DoesNotExist:
+            return model.objects.none(), None
 
-    # Query blog categories and posts
-    try:
-        exclusive_category = Category.objects.get(name='Exclusive')
-        exclusive_posts = BlogPost.objects.filter(category=exclusive_category).order_by('-date')[:5]
-    except Category.DoesNotExist:
-        exclusive_posts = []
+    # Fetch posts by category
+    exclusive_posts, exclusive_category = get_category_posts('Exclusive', limit=5)
+    global_news_posts, global_news_category = get_category_posts('Global News', limit=6)
+    sport_posts, _ = get_category_posts('Sports', limit=5)
+    tech_posts, _ = get_category_posts('Technology', limit=6)
+    video_posts, _ = get_category_posts('Videos', model=Video)
 
-    try:
-        global_news_category = Category.objects.get(name='Global News')
-        global_news_posts = BlogPost.objects.filter(category=global_news_category).order_by('-date')[:6]
-    except Category.DoesNotExist:
-        global_news_posts = []
-
-    try:
-        videos_category = Category.objects.get(name='Videos')
-        video_posts = Video.objects.filter(category=videos_category).order_by('-date')
-    except Category.DoesNotExist:
-        video_posts = []
-
-    try:
-        sport_category = Category.objects.get(name='Sports')
-        sport_posts = BlogPost.objects.filter(category=sport_category).order_by('-date')[:5]
-    except Category.DoesNotExist:
-        sport_posts = []
-
-    try:
-        tech_category = Category.objects.get(name='Technology')
-        tech_posts = BlogPost.objects.filter(category=tech_category).order_by('-date')[:6]
-    except Category.DoesNotExist:
-        tech_posts = []
-
-    # Ads from the AdBanner model (leaderboard and sidebar)
+    # Fetch ads
     leaderboard_ad = AdBanner.objects.filter(category='Leaderboard', active=True).first()
     sidebar_ad = AdBanner.objects.filter(category='Sidebar', active=True).first()
     home_ad = AdBanner.objects.filter(category='Home', active=True).first()
 
+    # Aggregate posts
     five_days_ago = timezone.now() - timedelta(days=5)
-    blogpost = BlogPost.objects.filter(date__lt=five_days_ago).order_by('-date')[:12]
-    trendpost = Trend.objects.filter(date__lt=five_days_ago).order_by('-date')[:12]
-    
     posts_from_five_days_ago = sorted(
-        chain(blogpost, trendpost),
+        chain(
+            BlogPost.objects.filter(date__lt=five_days_ago).order_by('-date')[:12],
+            Trend.objects.filter(date__lt=five_days_ago).order_by('-date')[:12]
+        ),
         key=lambda post: post.date,
         reverse=True
     )
 
-    all_posts = BlogPost.objects.all().order_by('-date')
+    # Hero posts and main post
     three_days_ago = timezone.now() - timedelta(days=3)
-    categories = Category.objects.all()
-    navbar_categories = Category.objects.filter(show_on_navbar=True).order_by('priority')
+    hero_posts = BlogPost.objects.filter(date__date=three_days_ago.date()).order_by('-date')[:5]
+    main_post = BlogPost.objects.order_by('?').first()
 
-    hero_posts = BlogPost.objects.filter(date__date=three_days_ago.date()).order_by('-date')
-    if hero_posts.count() > 5:
-        hero_posts = sample(list(hero_posts), 5)
+    # Exclude categories for non-exclusive posts
+    all_posts = BlogPost.objects.all().order_by('-date')
+    non_exclusive_posts = all_posts.exclude(category__in=filter(None, [exclusive_category, global_news_category]))[:4]
 
-    main_post = choice(all_posts) if all_posts.exists() else None
-
-    non_exclusive_posts = all_posts.exclude(category=exclusive_category) if exclusive_category else all_posts
-    non_exclusive_posts = non_exclusive_posts.exclude(category=global_news_category) if global_news_category else non_exclusive_posts
+    # Other data
     trends = Trend.objects.all().order_by('-date')[:5]
     shop = Product.objects.all().order_by('?')[:12]
+    navbar_categories = Category.objects.filter(show_on_navbar=True).order_by('priority')
 
-    # Context for the template
+    # Context
     context = {
         'main_post': main_post,
-        'hero_posts': hero_posts,
-        'non_exclusive_posts': non_exclusive_posts[:4],
+        'hero_posts': list(hero_posts),
+        'non_exclusive_posts': non_exclusive_posts,
         'exclusive_posts': exclusive_posts,
         'global_news_posts': global_news_posts,
         'posts_from_five_days_ago': posts_from_five_days_ago,
-        'categories': categories,
+        'categories': Category.objects.all(),
         'navbar_categories': navbar_categories,
         'video_posts': video_posts,
         'sport_posts': sport_posts,
@@ -90,9 +70,9 @@ def index(request):
         'shop': shop,
         'current_time': datetime.now(),
         'email': 'contact@scodynatenews.com',
-        'leaderboard_ad': leaderboard_ad, 
-        'sidebar_ad': sidebar_ad,  
-        'home_ad': home_ad,  
+        'leaderboard_ad': leaderboard_ad,
+        'sidebar_ad': sidebar_ad,
+        'home_ad': home_ad,
     }
 
     return render(request, 'home/index.html', context)
