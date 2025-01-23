b6c4315b38bc108616ea509e140bc43cc3365e6a
Maxonex <oluwamomis@gmail.com>
2025-01-23 23:42:33 +0100

    media file adjusted

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -168,7 +168,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -181,11 +180,15 @@ if not DEBUG:
 else:
     STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+# Media files (uploaded by users)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:
+    MEDIA_ROOT = '/var/media'  # Matches the mount path for the Render disk
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+#
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
